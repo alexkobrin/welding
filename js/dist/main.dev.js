aadf0b0b03d6c935ddb88d136014e95bed59465a
alexkobrin <storyday2012@gmail.com>
2020-08-03 12:38:20 +0200

    added v1.0

---
+++ js/dist/main.dev.js
@@ -0,0 +1,37 @@
+"use strict";
+
+$(function () {
+  $('.slider__inner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class=" slick-btn slick-prev"><img src="images/chevron-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class=" slick-btn slick-next"><img src="images/chevron-right.svg" alt=""></button>'
+  });
+  var secondarySlider = new Splide('.secondary-slider', {
+    rewind: true,
+    fixedWidth: 100,
+    fixedHeight: 64,
+    isNavigation: true,
+    gap: 10,
+    focus: 'center',
+    pagination: false,
+    cover: true,
+    arrows: false,
+    breakpoints: {
+      '600': {
+        fixedWidth: 66,
+        fixedHeight: 40
+      }
+    }
+  }).mount(); // Create the main slider.
+
+  var primarySlider = new Splide('.primary-slider', {
+    fixedWidth: 1170,
+    fixedHeight: 600,
+    pagination: false,
+    arrows: false,
+    cover: true
+  }); // Set the thumbnails slider as a sync target and then call mount.
+
+  primarySlider.sync(secondarySlider).mount();
+});
